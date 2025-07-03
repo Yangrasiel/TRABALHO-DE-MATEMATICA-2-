d7b559278c4ef9fd73146782935ea43a21c805ab
Yangrasiel <grasiel.borges@escola.pr.gov.br>
2025-07-03 08:29:57 -0300

    ok

---
+++ script.js
@@ -0,0 +1,16 @@
+function inserirNome(){
+   let nomeUsuario = prompt("Yan");
+   let elemento = document.querySelector("#Yangrasiel");
+   console.log(elemento);
+   elemento.textContent = nomeUsuario;
+}
+
+inserirNome();
+
+let linguagens = ["JavaScript", "Python","C++"]
+
+console.log (linguagens [0])
+
+console.log (linguagens [1])
+
+console.log (linguagens [2])
